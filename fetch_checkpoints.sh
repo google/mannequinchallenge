5b342a91db21ffc0d329ff459fde5867c46fda1b
Forrester Cole <fcole@google.com>
2019-05-09 15:14:41 -0400

    Adding script to fetch model checkpoints.

---
+++ fetch_checkpoints.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget https://storage.googleapis.com/mannequinchallenge-data/checkpoints/best_depth_Ours_Bilinear_inc_3_net_G.pth -O checkpoints/test_local/best_depth_Ours_Bilinear_inc_3_net_G.pth
